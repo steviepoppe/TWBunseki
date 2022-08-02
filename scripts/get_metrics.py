4034c5a985f3ce1c3d26938cf81c471222f62f64
Mariam Maarouf <mariam.maarouf@adjust.com>
2022-08-02 03:20:20 +0200

    Refactor metrics, add warnings

--- scripts/get_metrics.py
+++ scripts/get_metrics.py
@@ -29,12 +29,13 @@ import pandas as pd
 
 async def parse_tweets(args):
 
-	file_name = args['filename']
+	file_path = args['filename']
 	timezone = args['timezone']  # example: 'Asia/Tokyo', 'UTC'
 	keep_rt = not args['no_keep_rt']
 	analyze_datetime = not args['no_analyze_datetime']
 	analyze_users = not args['no_analyze_users']
 	analyze_hashtags = not args['no_analyze_hashtags']
+	analyze_hashtag_dates = analyze_hashtags
 	analyze_urls = args['analyze_urls']
 	exclude_twitter_urls = args['exclude_twitter_urls']
 	chunksize = args['chunk_size']
@@ -48,6 +49,10 @@ async def parse_tweets(args):
 	user_set = {}
 	media_set = {}
 	line_count = 0
+	skipped_tweets = {}  # reason: count
+	warnings = set()
+
+	file_name = file_path.split('/')[-1].replace('.csv', '')
 
 	save_file_name = file_name
 	if from_date:
@@ -57,7 +62,8 @@ async def parse_tweets(args):
 	
 	Path("./results/metrics_%s/" % save_file_name).mkdir(parents=True, exist_ok=True)
 
-	for chunk in pd.read_csv('./results/%s.csv' % file_name, encoding="utf-8", chunksize=chunksize, iterator=True):
+	for chunk in pd.read_csv(file_path, encoding="utf-8", chunksize=chunksize, iterator=True):
+		# time filtering and timezone conversion
 		chunk.created_at = pd.to_datetime(chunk.created_at, utc=True)
 		if timezone is not None:
 			chunk.created_at = chunk.created_at.dt.tz_convert(tz=timezone)
@@ -66,9 +72,35 @@ async def parse_tweets(args):
 		if to_date is not None:
 			chunk = chunk[chunk.created_at <= to_date]
 		chunk.created_at = chunk.created_at.apply(str)
+
+		# warnings
+		if 'hashtags' not in chunk and analyze_hashtags:
+			analyze_hashtags = False
+			analyze_hashtag_dates = False
+			warnings.add('"hashtags" column is required to analyze hashtags. Skipping.')
+		if 'created_at' not in chunk:
+			if analyze_hashtag_dates:
+				analyze_hashtag_dates = False
+				warnings.add('"created_at" column is required to analyze hashtags by date. Skipping.')
+			if analyze_datetime:
+				analyze_datetime = False
+				warnings.add('"created_at" column is required to analyze date and time metrics. Skipping.')
+		if 'text' not in chunk and analyze_urls:
+			analyze_urls = False
+			warnings.add('"text" column is required to analyze media URLs. Skipping.')	
+		if 'user_screen_name' not in chunk and analyze_users:
+			analyze_users = False
+			warnings.add('"user_screen_name" column is required to analyze user metrics. Skipping.')
+
 		for index, tweet in chunk.iterrows():
 			line_count += 1
-			is_retweet = 1 if tweet["is_retweet"] == True else 0
+			is_retweet = 1 if tweet.get('is_retweet', False) == True else 0
+
+			if is_retweet and not keep_rt:
+				reason = 'Retweets while keep-rt is set to False'
+				skipped_tweets[reason] = skipped_tweets.get(reason, 0) + 1
+				continue
+
 			if analyze_hashtags:
 				hashtag_metrics(tweet, hashtags, is_retweet)
 				hashtag_date_metrics(tweet, hashtag_dates, is_retweet)
@@ -76,7 +108,7 @@ async def parse_tweets(args):
 				date_metrics(tweet, date_set, is_retweet)
 				time_metrics(tweet, time_set, is_retweet)
 			if analyze_users:
-				user_metrics(tweet, user_set, is_retweet, keep_rt)
+				user_metrics(tweet, user_set, is_retweet)
 			if analyze_urls:
 				media_metrics(tweet, media_set, is_retweet)
 		if analyze_urls:
@@ -84,6 +116,12 @@ async def parse_tweets(args):
 		print('Processed %s lines.' % line_count)
 		
 	print('Processed total of %s lines.' % line_count)
+	if skipped_tweets:
+		for reason, number in skipped_tweets.items():
+			warnings.add(f'Skipped {number} tweet(s). Reason: "{reason}"')
+	for warning in warnings:
+		print(f'WARNING: {warning}')
+
 	if analyze_hashtags:
 		save_hashtag_metrics(hashtags, save_file_name)
 		save_hashtag_date_metrics(hashtag_dates, save_file_name)
@@ -94,7 +132,15 @@ async def parse_tweets(args):
 		save_user_metrics(user_set, save_file_name)
 	if analyze_urls:
 		save_media_metrics(media_set, save_file_name)
-			
+
+
+def get_initial_retweet_stat_matrix(is_retweet):
+	retweet_stats = [0, 0, [[],[]]]  # [number of og tweets, number of retweets, [list_of_og_tweeters, list_of_retweeters]]
+	retweet_stats[is_retweet] = 1
+	retweet_stats[not is_retweet] = 0
+	return retweet_stats
+
+
 def hashtag_metrics(tweet, hashtags, is_retweet):
 	if not pd.isna(tweet["hashtags"]):
 		c_hashtags = tweet["hashtags"].replace(",,",",").split(",") # possibility of empty strings joined = two commas
@@ -103,14 +149,10 @@ def hashtag_metrics(tweet, hashtags, is_retweet):
 				if hashtag in hashtags:
 					hashtags[hashtag][is_retweet] = hashtags[hashtag][is_retweet] + 1
 				else:
-					retweet_status = [0,0,0]
-					retweet_status[is_retweet] = 1
-					retweet_status[not is_retweet] = 0
-					retweet_status[2] = [0,0]
-					retweet_status[2][0] = []
-					retweet_status[2][1] = []
-					hashtags[hashtag] = retweet_status
-				hashtags[hashtag][2][is_retweet].append(tweet["user_screen_name"])
+					retweet_stats = get_initial_retweet_stat_matrix(is_retweet)
+					hashtags[hashtag] = retweet_stats
+				if 'user_screen_name' in tweet:
+					hashtags[hashtag][2][is_retweet].append(tweet["user_screen_name"])
 
 
 def hashtag_date_metrics(tweet, hashtag_dates, is_retweet):
@@ -122,16 +164,12 @@ def hashtag_date_metrics(tweet, hashtag_dates, is_retweet):
 				if hashtag_dates.get(hashtag, {}).get(tweet_created_month) is not None:
 					hashtag_dates[hashtag][tweet_created_month][is_retweet] = hashtag_dates[hashtag][tweet_created_month][is_retweet] + 1
 				else:
-					retweet_status = [0,0,0]
-					retweet_status[is_retweet] = 1
-					retweet_status[not is_retweet] = 0
-					retweet_status[2] = [0,0]
-					retweet_status[2][0] = []
-					retweet_status[2][1] = []
+					retweet_stats = get_initial_retweet_stat_matrix(is_retweet)
 					if hashtag not in hashtag_dates:
 						hashtag_dates[hashtag] = {}
-					hashtag_dates[hashtag][tweet_created_month] = retweet_status
-				hashtag_dates[hashtag][tweet_created_month][2][is_retweet].append(tweet["user_screen_name"])
+					hashtag_dates[hashtag][tweet_created_month] = retweet_stats
+				if 'user_screen_name' in tweet:
+					hashtag_dates[hashtag][tweet_created_month][2][is_retweet].append(tweet["user_screen_name"])
 
 def date_metrics(tweet, date_set, is_retweet):
 	#print(tweet["created_at"])
@@ -139,31 +177,23 @@ def date_metrics(tweet, date_set, is_retweet):
 #	print(tweet_created_date.strftime("%m/%d/%Y"))
 #	tweet_created_date = datetime.fromisoformat(tweet_created_date).strftime("%m/%d/%Y")
 	if not tweet_created_date in date_set:
-		retweet_status = [0,0,0]
-		retweet_status[is_retweet] = 1
-		retweet_status[not is_retweet] = 0
-		date_set[tweet_created_date] = retweet_status
-		retweet_status[2] = [0,0]
-		retweet_status[2][0] = []
-		retweet_status[2][1] = []
+		retweet_stats = get_initial_retweet_stat_matrix(is_retweet)
+		date_set[tweet_created_date] = retweet_stats
 	else:
 		date_set[tweet_created_date][is_retweet] += 1
-	date_set[tweet_created_date][2][is_retweet].append(tweet["user_screen_name"])
+	if 'user_screen_name' in tweet:
+		date_set[tweet_created_date][2][is_retweet].append(tweet["user_screen_name"])
 
 def time_metrics(tweet, time_set, is_retweet):
 	tweet_created_time = parser.parse(tweet["created_at"]).strftime("%H")  #change to %I %p for AM/PM
 
 	if not tweet_created_time in time_set:
-		retweet_status = [0,0,0]
-		retweet_status[is_retweet] = 1
-		retweet_status[not is_retweet] = 0
-		time_set[tweet_created_time] = retweet_status
-		retweet_status[2] = [0,0]
-		retweet_status[2][0] = []
-		retweet_status[2][1] = []
+		retweet_stats = get_initial_retweet_stat_matrix(is_retweet)
+		time_set[tweet_created_time] = retweet_stats
 	else:
 		time_set[tweet_created_time][is_retweet] += 1
-	time_set[tweet_created_time][2][is_retweet].append(tweet["user_screen_name"])
+	if 'user_screen_name' in tweet:
+		time_set[tweet_created_time][2][is_retweet].append(tweet["user_screen_name"])
 
 
 def media_metrics(tweet, media_set, is_retweet):
@@ -218,36 +248,28 @@ async def expand_url(session, url, max_redirect_depth):
 	error = expanded == ''
 	return url, expanded, error, domain
 
-def user_metrics(tweet, user_set, is_retweet, keep_rt):
-	if ("retweeted_status" in tweet) == False or keep_rt == True:
+def user_metrics(tweet, user_set, is_retweet):
+	if tweet["user_screen_name"] not in user_set:
 		user = {}
-		if tweet["user_screen_name"] not in user_set:
-			user["screen_name"] = tweet["user_screen_name"]
-			user["description"] = tweet["user_description"]
-			user["following_count"] = tweet["user_following_count"]
-			user["followers_count"] = tweet["user_followers_count"]
-			user["total_tweets"] = tweet["user_total_tweets"]
-			user["created_at"] = tweet["user_created_at"]
-			user["total_in_data_set"] = [0,0]
-			user["total_in_data_set"][is_retweet] = 1
-			## Note, if full retweet count is preferred, replace "retweet_count_dataset" with "retweet_count_listed"
-			if "retweet_count_dataset" in tweet:
-				user["total_times_retweeted_in_dataset"] = int(tweet["retweet_count_dataset"])
-			user_set[tweet["user_screen_name"]] = user
+		user["screen_name"] = tweet["user_screen_name"]
+		user["description"] = tweet.get("user_description", "")
+		user["following_count"] = tweet.get("user_following_count", -1)
+		user["followers_count"] = tweet.get("user_followers_count", -1)
+		user["total_tweets"] = tweet.get("user_total_tweets", -1)
+		user["created_at"] = tweet.get("user_created_at", '')
+		user["total_in_data_set"] = [0,0]
+		user["total_in_data_set"][is_retweet] = 1
+		user_set[tweet["user_screen_name"]] = user
 
-		else:			
-			user_set[tweet["user_screen_name"]]["total_in_data_set"][is_retweet] += 1
-			if "retweet_count_dataset" in tweet:
-				user_set[tweet["user_screen_name"]]["total_times_retweeted_in_dataset"] += int(tweet["retweet_count_dataset"])
-			if tweet["user_following_count"] > user_set[tweet["user_screen_name"]]["following_count"]:
-				user_set[tweet["user_screen_name"]]["following_count"] 
-			if tweet["user_followers_count"] > user_set[tweet["user_screen_name"]]["followers_count"]:
-				user_set[tweet["user_screen_name"]]["followers_count"] 
-			if tweet["user_total_tweets"] > user_set[tweet["user_screen_name"]]["total_tweets"]:
-				user_set[tweet["user_screen_name"]]["total_tweets"] 
+	else:			
+		user_set[tweet["user_screen_name"]]["total_in_data_set"][is_retweet] += 1
+		if tweet["user_following_count"] > user_set[tweet["user_screen_name"]]["following_count"]:
+			user_set[tweet["user_screen_name"]]["following_count"] 
+		if tweet["user_followers_count"] > user_set[tweet["user_screen_name"]]["followers_count"]:
+			user_set[tweet["user_screen_name"]]["followers_count"] 
+		if tweet["user_total_tweets"] > user_set[tweet["user_screen_name"]]["total_tweets"]:
+			user_set[tweet["user_screen_name"]]["total_tweets"] 
 
-		#if tweet["user_screen_name"] not in unique_users[is_retweet]:
-		#	unique_users[is_retweet].append(tweet["user_screen_name"])
 
 def save_hashtag_metrics(hashtags, file_name):
 	with open('./results/metrics_%s/%s_hashtags.csv' % (file_name, file_name), 
@@ -264,6 +286,7 @@ def save_hashtag_metrics(hashtags, file_name):
 				str(len(retweet)), str(len(unique)), str(len(normal) + len(unique))])
 	print("Finished. Saved to ./results/metrics_%s/%s_hashtags.csv" % (file_name, file_name))
 
+
 def save_hashtag_date_metrics(hashtag_dates, file_name):
 	with open('./results/metrics_%s/%s_hashtag_dates.csv' % (file_name, file_name), 
 		mode='w', encoding="utf-8",newline='') as file_hashtags:
@@ -279,6 +302,7 @@ def save_hashtag_date_metrics(hashtag_dates, file_name):
 				writer_hashtags.writerow([hashtag, month, value[0], value[1], value[0] + value[1], str(len(normal)), 
 					str(len(retweet)), str(len(unique)), str(len(normal) + len(unique))])
 	print("Finished. Saved to ./results/metrics_%s/%s_hashtag_dates.csv" % (file_name, file_name))
+
 
 def save_date_metrics(date_set, file_name):
 	with open('./results/metrics_%s/%s_date.csv' % (file_name, file_name), 
@@ -300,6 +324,7 @@ def save_date_metrics(date_set, file_name):
 
 	print("Finished. Saved to ./results/metrics_%s/%s_date.csv" % (file_name, file_name))
 
+
 def save_time_metrics(time_set, file_name):
 	with open('./results/metrics_%s/%s_time.csv' % (file_name, file_name), 
 		mode='w', encoding="utf-8",newline='') as file_time:
@@ -315,6 +340,7 @@ def save_time_metrics(time_set, file_name):
 				str(len(normal)), str(len(retweet)), str(len(unique)), str(len(normal) + len(retweet))])
 	print("Finished. Saved to ./results/metrics_%s/%s_time.csv" % (file_name, file_name))
 
+
 def save_media_metrics(media_set, file_name):
 	with open('./results/metrics_%s/%s_media.csv' % (file_name, file_name),
 		mode='w', encoding='utf-8', newline='') as file_media:
@@ -328,37 +354,19 @@ def save_media_metrics(media_set, file_name):
 				writer_media.writerow([url, '', '', str(True), str(value['metrics'][0]), str(value['metrics'][1])])
 	print('Finished. Saved to ./results/metrics_%s/%s_media.csv' % (file_name, file_name))
 
+
 def save_user_metrics(user_set, file_name):
 	with open('./results/metrics_%s/%s_users.csv' % (file_name, file_name), 
 		mode='w', encoding="utf-8",newline='') as file_users:
 		writer_users = csv.writer(file_users)
-			
-
-		if "total_times_retweeted_in_dataset" in list(user_set.values())[0]:
-			writer_users.writerow(["screen_name", "total_posted_normal","total_posted_retweets","total_posted", 
-				"user_description","user_following_count", "user_followers_count", "user_total_tweets", 
-				"total_times_retweeted_in_dataset","user_created_at"])
-			with open('./results/metrics_%s/%s_old_retweets.csv' % (file_name, file_name), 
-				mode='r', encoding="utf-8") as old_retweets:
-				csv_reader = csv.DictReader(old_retweets)
-				for tweet in csv_reader:
-					if tweet["user_screen_name"] in user_set:
-						user_set[tweet["user_screen_name"]]["total_times_retweeted_in_dataset"] += int(tweet["retweet_count"])
-		else:
-			writer_users.writerow(["screen_name", "total_posted_normal","total_posted_retweets","total_posted", 
-				"user_description","user_following_count", "user_followers_count", "user_total_tweets","user_created_at"])
+		writer_users.writerow(["screen_name", "total_posted_normal","total_posted_retweets","total_posted", 
+			"user_description","user_following_count", "user_followers_count", "user_total_tweets","user_created_at"])
 
 		for a in user_set:
 			user = user_set[a]
-			if "total_times_retweeted_in_dataset" not in user:
-				writer_users.writerow([user["screen_name"],user["total_in_data_set"][0],user["total_in_data_set"][1], 
-					(user["total_in_data_set"][0] + user["total_in_data_set"][1]), user["description"],
-					user["following_count"],user["followers_count"],user["total_tweets"],user["created_at"]])
-			else:
-				writer_users.writerow([user["screen_name"],user["total_in_data_set"][0],user["total_in_data_set"][1], 
-					(user["total_in_data_set"][0] + user["total_in_data_set"][1]), user["description"],
-					user["following_count"],user["followers_count"],user["total_tweets"],
-					user["total_times_retweeted_in_dataset"],user["created_at"]])
+			writer_users.writerow([user["screen_name"],user["total_in_data_set"][0],user["total_in_data_set"][1], 
+				(user["total_in_data_set"][0] + user["total_in_data_set"][1]), user["description"],
+				user["following_count"],user["followers_count"],user["total_tweets"],user["created_at"]])
 
 	print("Finished. Saved to ./results/metrics_%s/%s_users.csv" % (file_name, file_name))
 
@@ -370,7 +378,7 @@ if __name__ == '__main__':
 		'--filename',
 		type=str,
 		required=True,
-		help='Name of the csv file inside ./results/ without .csv (e.g. MyResults would point to ./results/MyResults.csv)',
+		help='Full or relative path to the csv file. E.g. results/my_data.csv',
 	)
 	p.add_argument(
 		'-c',
