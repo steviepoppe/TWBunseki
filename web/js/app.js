9bde57069c30387a5512ca820eea257997d54ed4
Mariam Maarouf <mariam.maarouf@adjust.com>
2022-08-06 22:24:08 +0200

    Download zip

---
+++ web/js/app.js
@@ -0,0 +1,57 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+      return {
+        zipped_filename: 'twbunseki_scripts',
+        scripts: [
+            {
+                name: 'search',
+                desc: 'Fetch data from Twitter API',
+                filename: 'twitter_search.py',
+                src: 'scripts/twitter_search.py'
+            },
+            {
+                name: 'filter',
+                desc: 'Filter csv by a number of configurable variables',
+                filename: 'filter.py',
+                src: 'scripts/filter.py'
+            },
+            {
+                name: 'categorize',
+                desc: 'Categorize data rows based on given keyword map',
+                filename: 'categorize.py',
+                src: 'scripts/categorize.py'
+            },
+            {
+                name: 'analyze',
+                desc: 'Analyze data using an number of configurable methods',
+                filename: 'get_metrics.py',
+                src: 'scripts/get_metrics.py'
+            }
+        ]
+      }
+    },
+    methods: {
+        download_zipped() {
+            const comp = this;
+            var zip = JSZip();
+            var folder = zip.folder(comp.zipped_filename);
+
+            for (const i in comp.scripts) {
+                var fetched = fetch(comp.scripts[i].src).then(response => response.blob());
+                folder.file(comp.scripts[i].filename, fetched);
+            }
+
+            const blob = zip.generateAsync({type:'blob'}).then((blob) => {
+                const url = URL.createObjectURL(blob);
+                var a = document.createElement('a');
+                a.href = url;
+                a.download = comp.zipped_filename + '.zip';
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+            });
+        }
+    }
+}).mount('#app');
